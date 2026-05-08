dfa068207b2e3b23179de9df3febab894707ea42
Roberto Rubio <roberto_rubio98@outlook.com>
2026-05-08 01:55:32 -0600

    :sparkles: Add to ChatModule.kt provideChatCloudRepository

--- app/src/main/java/com/example/TensorFlowObjectDetector/di/ChatModule.kt
+++ app/src/main/java/com/example/TensorFlowObjectDetector/di/ChatModule.kt
@@ -2,11 +2,16 @@ package com.example.TensorFlowObjectDetector.di
 
 import android.app.Application
 import com.example.TensorFlowObjectDetector.data.chat.BuildConfig
+import com.example.TensorFlowObjectDetector.data.chat.CloudRunChatApiService
+import com.example.TensorFlowObjectDetector.data.chat.CloudRunChatRepository
+import com.example.TensorFlowObjectDetector.data.chat.CloudRunRetrofitClient
 import com.example.TensorFlowObjectDetector.data.chat.DirectGeminiChatRepository
 import com.example.TensorFlowObjectDetector.data.chat.GeminiApiService
 import com.example.TensorFlowObjectDetector.data.chat.GeminiRetrofitClient
+import com.example.TensorFlowObjectDetector.tensorchat.ChatCloudRepository
 import com.example.TensorFlowObjectDetector.tensorchat.ChatRepository
 import com.example.TensorFlowObjectDetector.tensorchat.ChatUseCaseFactory
+import com.example.TensorFlowObjectDetector.tensorchat.SendPromptCloudUseCase
 import com.example.TensorFlowObjectDetector.tensorchat.SendPromptUseCase
 import dagger.Module
 import dagger.Provides
@@ -24,6 +29,22 @@ object ChatModule {
 
     @Provides
     @Singleton
+    fun provideCloudRunChatApiService(): CloudRunChatApiService = CloudRunRetrofitClient.api
+
+    @Provides
+    @Singleton
+    fun provideChatCloudRepository(
+        application: Application,
+        api: CloudRunChatApiService
+    ): ChatCloudRepository {
+        return CloudRunChatRepository(
+            context = application,
+            api = api
+        )
+    }
+
+    @Provides
+    @Singleton
     fun provideChatRepository(
         application: Application,
         api: GeminiApiService
@@ -38,5 +59,10 @@ object ChatModule {
     @Provides
     fun provideSendPromptUseCase(chatRepository: ChatRepository): SendPromptUseCase {
         return ChatUseCaseFactory.createSendPromptUseCase(chatRepository)
+    }
+
+    @Provides
+    fun provideSendPromptCloudUseCase(chatCloudRepository: ChatCloudRepository): SendPromptCloudUseCase {
+        return ChatUseCaseFactory.createSendPromptCloudUseCase(chatCloudRepository)
     }
 }
