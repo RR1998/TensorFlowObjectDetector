9d2bd7a4b479353b2fc8f2af4814ec22f5df2165
Roberto Rubio <roberto_rubio98@outlook.com>
2026-05-08 01:51:22 -0600

    :sparkles: Add CLOUD_RUN_BASE_URL to BuildConfig.java

--- data/chat/build/generated/source/buildConfig/debug/com/example/TensorFlowObjectDetector/data/chat/BuildConfig.java
+++ data/chat/build/generated/source/buildConfig/debug/com/example/TensorFlowObjectDetector/data/chat/BuildConfig.java
@@ -8,5 +8,7 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.example.TensorFlowObjectDetector.data.chat";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
+  public static final String CLOUD_RUN_BASE_URL = "https://smart-image-chat-api-743038959932.us-central1.run.app/";
+  // Field from default config.
   public static final String GEMINI_API_KEY = "AIzaSyB3kk_Kmfh9DDLfHL16uI2xjWsK22YmEWw";
 }
